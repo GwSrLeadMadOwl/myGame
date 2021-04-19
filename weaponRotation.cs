ef89c8e755f1be3b0e967b96f2f0b4fec1026c58
GwSrLeadMadOwl <60887322+GwSrLeadMadOwl@users.noreply.github.com>
2021-04-19 08:52:33 +0300

    Update aiming system

--- weaponRotation.cs
+++ weaponRotation.cs
@@ -7,6 +7,8 @@ public class weaponRotation : MonoBehaviour
     public float aimingSpeed = 0.01f;
     public GameObject aimTarget;
 
+    Vector3 aimTransPos;
+
     Quaternion rotGoal;
     Vector3 direction;
 
@@ -14,14 +16,36 @@ public class weaponRotation : MonoBehaviour
     public bool zoomInZone = false;
     public bool zoomOutZone = false;
 
+    private void Awake()
+    {
+        aimTransPos = aimTarget.transform.localPosition;
+    }
+
     private void Update()
     {
+        var aimY = -Input.GetAxis("Rightstick Vertical") * aimingSpeed * 200 * Time.deltaTime;
+        var aimX = -Input.GetAxis("Rightstick Horizontal") * aimingSpeed * 200 * Time.deltaTime;
+
+        //limit aim movements
+        Vector3 clampedAim = aimTarget.transform.localPosition;
+        clampedAim.y = Mathf.Clamp(clampedAim.y, -2.0f, 4.0f);
+        clampedAim.x = Mathf.Clamp(clampedAim.x, -5.0f, 5.0f);
+        aimTarget.transform.localPosition = clampedAim;
+
+        //raturn aim to the origin
+        if (Input.GetAxis("Rightstick Vertical") == 0 && Input.GetAxis("Rightstick Horizontal") == 0)
+        {
+            aimTarget.transform.localPosition = Vector3.Lerp(clampedAim, aimTransPos, aimingSpeed * 50 * Time.deltaTime);
+        }
+
         Vector3 aimPos = Camera.main.WorldToScreenPoint(aimTarget.transform.position);
         Ray ray = Camera.main.ScreenPointToRay(aimPos);
         RaycastHit hit;
 
         if (aimTarget.transform.localPosition.x < .8 && aimTarget.transform.localPosition.x > -.8 && aimTarget.transform.localPosition.y > -1 && aimTarget.transform.localPosition.y < 1.2)
         {
+            aimTarget.transform.Translate(aimX, 0, aimY);
+
             aimTarget.SetActive(false);
 
             Quaternion idle = Quaternion.Euler(0, 0, -90);
@@ -30,15 +54,18 @@ public class weaponRotation : MonoBehaviour
         else
         {
             aimTarget.SetActive(true);
+
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << LayerMask.NameToLayer("Enemy")))
             {
                 //print("Piu piu");
                 direction = (hit.point - transform.position).normalized;
+                aimTarget.transform.Translate(0, 0, 0);
             }
             else
             {
                 //print("I'm looking at nothing");
                 direction = (aimTarget.transform.position - transform.position).normalized;
+                aimTarget.transform.Translate(aimX, 0, aimY);
             }
 
             //fixed offset rotation
